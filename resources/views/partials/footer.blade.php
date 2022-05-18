8f2014479401bb0a1d8a79364df248907b52d791
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-05-19 01:51:49 +0200

    add main footer and bottom footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -40,4 +40,62 @@
         </li>
       </ul>
     </div>
+     <!-- link con dc logo grande -->
+     <div class="main-footer">
+      <div class="link-list">
+        <div class="link-image">
+          <ul>
+            <h3>dc comics</h3>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">Tv</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">News</a></li>
+          </ul>
+          <ul>
+            <h3>dc</h3>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Privacy policy(new)</a></li>
+            <li><a href="#">Ad Choices</a></li>
+            <li><a href="#">Advertising</a></li>
+            <li><a href="#">Subsciptions</a></li>
+            <li><a href="#">Talent Workshops</a></li>
+            <li><a href="#">CPSC Certificates</a></li>
+            <li><a href="#">Ratings</a></li>
+            <li><a href="#">shop Help</a></li>
+            <li><a href="#">contact Us</a></li>
+          </ul>
+          <ul>
+            <h3>sites</h3>
+            <li><a href="#">DC</a></li>
+            <li><a href="#">MAD Magazine</a></li>
+            <li><a href="#">DC Kids</a></li>
+            <li><a href="#">DC Universe</a></li>
+            <li><a href="#">DC Power Visa</a></li>
+          </ul>
+          <ul>
+            <h3>shop</h3>
+            <li><a href="#">Shop DC</a></li>
+            <li><a href="#">Shop DC Collectibles</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+      <!-- button sign-up e icone social -->
+      <div class="bottom-footer">
+      <div class="connect">
+        <button class="btn">Sign-up now</button>
+
+        <div class="icons">
+          <h3>Follow us</h3>
+          <img src="../images/footer-facebook.png" alt="facebook" />
+          <img src="../images/footer-twitter.png" alt="twitter" />
+          <img src="../images/footer-youtube.png" alt="youtube" />
+          <img src="../images/footer-pinterest.png" alt="pinterest" />
+          <img src="../images/footer-periscope.png" alt="periscope" />
+        </div>
+      </div>
+    </div>
 </footer>
