b9dee7bc1efeeb6298ac41f90f169493aa667dd6
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-05-18 19:32:26 +0200

    add header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,27 +1,31 @@
-@extends('layouts.app') // estendo il layout
-
-@secttion('title')
-    DC Comics
-@endsection
-
-@section('content header')
-
-    <div class="container-header">
+<header>
         <div class="top-bar">
-            <h3>DC POWER&trade; visa&reg;</h3>
+            <h3>DC POWER&trade; VISA&reg;</h3>
             <h3>ADDICTIONAL DC SITES</h3>
         </div>
         <div class="menu-navbar">
             <div class="container-logo">
-                <img src="../assets/dc-logo.png" alt="DC-logo">
+                <img src="../images/dc-logo.png" alt="DC-logo">
             </div>
             <div class="container-nav-list">
-
+                <ul>
+                    <li><a href="#">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEO</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li>
+                </ul>
+            </div>
+            <div class="search">
+            <input class="search-bar" type="text" placeholder="Search">
+            <button type="submit"><i class="fa fa-search"></i></button>
             </div>
         </div>
-    </div>
+        <div class="jumbotron"></div>
+</header
 
-
-
-@endsection
 
